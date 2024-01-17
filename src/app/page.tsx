27d249b741165e252259f66af07256ffaff1a2d0
JENNIFER DE LEO <deleo.jennifer@yahoo.fr>
2024-01-17 19:53:50 +0100

    fix error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,10 @@ import { getPopularMovies, getTopRatedMovies } from "@/libs/api/movies";
 import { getPopularTvShows, getTopRatedTvShows } from "@/libs/api/tvshows";
 
 export default async function Home() {
-  const { results: popularMovies } = await getPopularMovies();
+  const { results: popularMovies } = await getPopularMovies(1);
   const { results: topRatedMovies } = await getTopRatedMovies();
 
-  const { results: popularTvShows } = await getPopularTvShows();
+  const { results: popularTvShows } = await getPopularTvShows(1);
   const { results: topRatedTvShows } = await getTopRatedTvShows();
 
   return (
